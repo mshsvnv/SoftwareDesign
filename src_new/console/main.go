e614a1dde58e9c48b7aa14b521836630af920891
mshsvnv <mshsvnv@mail.ru>
2024-04-29 20:51:14 +0300

    some progress)))

--- src_new/console/main.go
+++ src_new/console/main.go
@@ -1,58 +1,34 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"src_new/internal/dto"
-	"src_new/internal/repository"
-	"src_new/internal/service"
+
+	"github.com/rivo/tview"
 )
 
+const pageCount = 5
+
 func main() {
-	repo := repository.NewUserRepository()
-	serv := service.NewUserService(repo)
-
-	ctx := context.Background()
-
-	_, _ = serv.Register(ctx, &dto.RegisterReq{
-		Name:     "Masha",
-		Surname:  "Savinova",
-		Email:    "mail@.ru",
-		Password: "123456",
-	})
-
-	repoCart := repository.NewCartRepository()
-	servCart := service.NewCartService(repoCart)
-
-	_, err := servCart.GetCartByUserID(ctx, 0)
-
-	if err != nil {
-		fmt.Println(err.Error())
+	app := tview.NewApplication()
+	pages := tview.NewPages()
+	for page := 0; page < pageCount; page++ {
+		func(page int) {
+			pages.AddPage(fmt.Sprintf("page-%d", page),
+				tview.NewModal().
+					SetText(fmt.Sprintf("This is page %d. Choose where to go next.", page+1)).
+					AddButtons([]string{"Next", "Quit"}).
+					SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+						if buttonIndex == 0 {
+							pages.SwitchToPage(fmt.Sprintf("page-%d", (page+1)%pageCount))
+						} else {
+							app.Stop()
+						}
+					}),
+				false,
+				page == 0)
+		}(page)
 	}
-
-	cart, _ := servCart.AddRacket(ctx, &dto.AddRacketReq{
-		UserID:   0,
-		RacketID: 1,
-		Quantity: 2,
-	})
-
-	for _, line := range cart.Lines {
-		fmt.Printf("Racket ID: %d Quantity: %d\n", line.RacketID, line.Quantity)
-	}
-
-	cart, _ = servCart.AddRacket(ctx, &dto.AddRacketReq{
-		UserID:   0,
-		RacketID: 2,
-		Quantity: 2,
-	})
-
-	// cart, err := servCart.GetCartByUserID(ctx, 0)
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-
-	for _, line := range cart.Lines {
-		fmt.Printf("Racket ID: %d Quantity: %d\n", line.RacketID, line.Quantity)
+	if err := app.SetRoot(pages, true).SetFocus(pages).Run(); err != nil {
+		panic(err)
 	}
 }
